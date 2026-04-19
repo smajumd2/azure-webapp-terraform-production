3f610fcf95905750744cd6c1d1696ff1bd9046dc
sumit-majumder-dev <160128905+smajumd2@users.noreply.github.com>
2026-04-19 18:21:31 +0530

    Change SKU name from B1 to S1

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "S1"
 }
 
 # Azure Web App
