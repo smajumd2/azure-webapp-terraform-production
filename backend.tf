511cd593aa18813460bf53167fb48235d9052b22
sumit-majumder-dev <160128905+smajumd2@users.noreply.github.com>
2026-04-19 17:57:22 +0530

    Enable Azure AD authentication for Terraform backend

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
         storage_account_name = "cinuatwhereismyworld"
         container_name = "content"
         key = "test.webapp.tfstate"
+        use_azuread_auth     = true
     }
 
 }
